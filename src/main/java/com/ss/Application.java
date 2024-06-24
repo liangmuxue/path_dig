83d616ea922a3ceca2ea68e88a2add1713c3b534
JF <13121525+fujinfeng@user.noreply.gitee.com>
2024-06-24 15:12:57 +0800

    新建项目

---
+++ src/main/java/com/ss/Application.java
@@ -0,0 +1,18 @@
+package com.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.ss.manaplat.common.web
+ *
+ * @author ${USER}
+ * @create ${DATE}
+ * @email fujinfeng@ss-cas.com
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
